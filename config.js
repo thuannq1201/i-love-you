933edc5931e3f21642507f7d1bbb286c1dea482a
thuannq1201 <mscchannel02@gmail.com>
2022-01-08 20:00:12 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Làm người yêu anh nhé baby",
+    introTitle: 'Em đã sẵn sàng chưa',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'sẵn sàng',
+    title: 'Lấy hết can đảm để hỏi em một câu 🥰',
+    desc: 'Làm người yêu anh nha !!',
+    btnYes: 'Yessss <33',
+    btnNo: 'Noo :3',
+    question: 'Tết này cho anh sang nhà nha!!!!',
+    btnReply: 'Send',
+    reply: 'yesss',
+    mess: 'Hí Hí ',
+    messDesc: 'yêu lắm kerrr <333',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: "http://www.daoquangduong.vn/meo-chat-thit-ga-khong-nat-cho-cac-anh-chang-ve-ra-mat-bo-vo"
+}
